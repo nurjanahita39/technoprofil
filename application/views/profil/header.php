e1b9ca6da347b7b817ab4bbb157fbe766a3dc249
Ita Nurjanah <ithajannah039@gmail.com>
2018-12-22 12:45:28 +0700

    header

--- application/views/profil/header.php
+++ application/views/profil/header.php
@@ -15,10 +15,7 @@
 
     <!-- Custom fonts for this template -->
     <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/vendor/fontawesome-free/css/all.css" />
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
 
     <!-- Custom styles for this template -->
     <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/agency.min.css" />
