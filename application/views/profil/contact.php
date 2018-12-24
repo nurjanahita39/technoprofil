12cff4b3ef59bd1d7497079cd6c461a1457837c6
Unknown <davidbristian@gmail.com>
2018-12-25 05:29:55 +0700

    kirim ulang contact
    contact aja coba

--- application/views/profil/contact.php
+++ application/views/profil/contact.php
@@ -3,7 +3,7 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-12 text-center">
-            <h2 class="section-heading text-uppercase">Contact Us</h2>
+            <h2 class="section-heading text-uppercase">Hubungi Kami</h2>
             <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
           </div>
         </div>
@@ -35,7 +35,7 @@
                 <div class="clearfix"></div>
                 <div class="col-lg-12 text-center">
                   <div id="success"></div>
-                  <button id="sendMessageButton" class="btn btn-primary btn-xl text-uppercase" type="submit">Send Message</button>
+                  <button id="sendMessageButton" class="btn btn-primary btn-xl text-uppercase" type="submit">Kirim Pesan</button>
                 </div>
               </div>
             </form>
