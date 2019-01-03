ddef990cda2fc58e7714893f4eac9ebe99ef0b96
Ita Nurjanah <ithajannah039@gmail.com>
2019-01-03 14:12:44 +0700

    little edit

--- application/views/profil/services.php
+++ application/views/profil/services.php
@@ -15,7 +15,7 @@
             <p class="text-muted">Aplikasi Android berupa software yang hanya dioperasikan secara individu ataupun aplikasi yang dapat terhubung dengan device ataupun <i>mechine</i> untuk monitoring dan <i>controll</i> </p>
           </div>
           <div class="col-md-4">
-            <h4 class="service-heading">Web Design</h4>
+            <h4 class="service-heading">Web Aplikasi dan Desainn</h4>
              <img class="img-fluid" src="<?php echo base_url() ?>assets/img/Services/web.png"width="170px" height="170px" alt="">
             <p class="text-muted">Aplikasi berupa Website dan Web <i>mobile</i> yang bisa dijalankan secara <i>online/offline</i> yaitu menggunakan teknologi baru yang dimunculkan oleh google</p>
             <link href="">
