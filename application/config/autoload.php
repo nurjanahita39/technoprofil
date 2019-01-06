ee5fcd86888e01dd24b371e5e863a7b082b62451
Ita Nurjanah <ithajannah039@gmail.com>
2019-01-06 10:43:34 +0700

    login admin

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -5,7 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $autoload['packages'] = array();
 
 
-$autoload['libraries'] = array('database');
+$autoload['libraries'] = array('database','session');
 
 
 $autoload['drivers'] = array();
